4029a9814d39af88120254111bd5310a40221ad0
Eduardo Moreira <81780428+edudzms@users.noreply.github.com>
2021-04-01 20:16:11 -0300

    AluraFlix

---
+++ aluraflix/README.markdown
@@ -0,0 +1,5 @@
+# AluraFlix
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/eduardo-moreira/pen/bGgVywp](https://codepen.io/eduardo-moreira/pen/bGgVywp).
+
+
---
+++ aluraflix/dist/script.js
@@ -0,0 +1,21 @@
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BOTg4ZTNkZmUtMzNlZi00YmFjLTk1MmUtNWQwNTM0YjcyNTNkXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZGI5MjBmYzYtMzJhZi00NGI1LTk3MzItYjBjMzcxM2U3MDdiXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMjIyNTQ5NjQ1OV5BMl5BanBnXkFtZTcwODg1MDU4OA@@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+for (var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i] + ">")
+}
+
+
+//var filmes = ["Matrix","A Sociedade do Anel","O Cavaleiro das Trevas","As Duas Torres","O Retorno do Rei"]
+
+//for (var i = 0 ; i < filmes.length ; i++) 
+//{
+//  console.log (filmes[i])
+//}
+
+//filmes.push("Matrix")
+//filmes.push("A Sociedade do Anel")
+
+//console.log (filmes[0])
+//console.log (filmes[1])
+
+//console.log (filmes.length)
